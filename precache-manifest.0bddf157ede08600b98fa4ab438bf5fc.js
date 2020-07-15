24094fb1ce767c18072f9f276ff895bd2a3fd680
Sam Symonds <samuelsymonds2017@gmail.com>
2020-07-15 17:48:59 +0100

    Updates

---
+++ precache-manifest.0bddf157ede08600b98fa4ab438bf5fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c592ceb3e523c991f592789fe907e1",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "363fb15a73c29cf6499d",
+    "url": "/portfolio2020/static/css/main.235ec4a9.chunk.css"
+  },
+  {
+    "revision": "244878dcdaa99f8b3f63",
+    "url": "/portfolio2020/static/js/2.650b80be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio2020/static/js/2.650b80be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363fb15a73c29cf6499d",
+    "url": "/portfolio2020/static/js/main.0d862d65.chunk.js"
+  },
+  {
+    "revision": "6ad797aa38266a95762d",
+    "url": "/portfolio2020/static/js/runtime-main.26a4754f.js"
+  },
+  {
+    "revision": "2324eaf61d4a961d32bdaba3f4304201",
+    "url": "/portfolio2020/static/media/email_icon.2324eaf6.svg"
+  },
+  {
+    "revision": "e37fa66f0ba2d6d7afec8546c35cbf43",
+    "url": "/portfolio2020/static/media/github_icon.e37fa66f.svg"
+  },
+  {
+    "revision": "a81f25c23ac3250a5b1c63ae19b4d48b",
+    "url": "/portfolio2020/static/media/linkedin_icon.a81f25c2.svg"
+  },
+  {
+    "revision": "8e3a0fdc6ef93f53fc9830dd7f0c521c",
+    "url": "/portfolio2020/static/media/medium_icon.8e3a0fdc.svg"
+  },
+  {
+    "revision": "279fc7eead3d57a0e488eaa7ac3f5ba1",
+    "url": "/portfolio2020/static/media/movie-reference-app.279fc7ee.png"
+  },
+  {
+    "revision": "2ec5c3e7671771be3a36f35bab379214",
+    "url": "/portfolio2020/static/media/pokedex-app.2ec5c3e7.png"
+  },
+  {
+    "revision": "f02969dfcff317a9276b8c7e303d517b",
+    "url": "/portfolio2020/static/media/twitter_icon.f02969df.svg"
+  }
+]);
